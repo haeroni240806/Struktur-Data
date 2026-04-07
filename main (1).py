c88e44668f1a5538b4ce511bd2c78a5202997b95
haeroni240806 <haeroni488@gmail.com>
2026-04-07 13:31:24 +0800

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,24 @@
+# Input nama
+nama = input("Masukan nama anda: ")
+
+# Validasi nama (misalnya tidak boleh kosong)
+if nama.strip() != "":
+    print(f"Selamat datang {nama}")
+else:
+    print("Program selesai")
+    exit()
+
+# Input umur
+umur = int(input("Masukan umur anda: "))
+
+# Percabangan kondisi umur
+if umur >= 18:
+    print("Anda cukup umur")
+elif umur < 0:
+    print("Anda belum lahir")
+elif umur < 18:
+    print("Anda belum cukup umur")
+elif umur > 60:
+    print("Banyakin ibadah, bentar lagi mati")
+
+print("Program selesai")
